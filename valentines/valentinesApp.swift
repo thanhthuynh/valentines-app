f8681aae636fc37ebe51f4212c167a754b2bd8a3
Thanh <thanhthuynh7@gmail.com>
2026-02-05 23:00:10 -0500

    Initial Commit

---
+++ valentines/valentinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  valentinesApp.swift
+//  valentines
+//
+//  Created by Thanh Huynh on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct valentinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ valentinesTests/valentinesTests.swift
@@ -0,0 +1,17 @@
+//
+//  valentinesTests.swift
+//  valentinesTests
+//
+//  Created by Thanh Huynh on 2/5/26.
+//
+
+import Testing
+@testable import valentines
+
+struct valentinesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
